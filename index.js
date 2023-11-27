5de321f5f5ec1d899e960b4c017a2eb5beb6a763
Replit user <>
2023-11-27 02:41:48 +0000

    add mongoDB and connect to database collection

--- index.js
+++ index.js
@@ -2,18 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-let bodyParser = require('body-parser');
+
+const { MongoClient } = require('mongodb');
+const mySecret = process.env['MONGO_URI'];
+const client = new MongoClient(mySecret);
+
+const db = client.db('urlshortener');
+const urls = db.collection('urls');
 
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -24,6 +29,15 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl', function(req, res) {
+  let body = req.body;
+  res.json({
+    your_duplicate_body: 'success', 
+  });
+
+  console.log(body);
+});
+                              
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
