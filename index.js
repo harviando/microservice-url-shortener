c9ef006523e3d3e27430897d062e3a9b1e970beb
Replit user <>
2023-11-27 02:22:27 +0000

    reset project

--- index.js
+++ index.js
@@ -2,13 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const dns = require('dns');
-const urlParser = require('url');
 let bodyParser = require('body-parser');
-let mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
 
-mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -27,26 +22,6 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
-});
-
-// Schema Init
-const Schema = mongoose.Schema;
-
-const urlSchema = new Schema ({
-  original_link: {
-    type: String,
-    required: true,
-  },
-  short_link: Number,
-});
-
-// Model Init
-let Url = mongoose.model('url', urlSchema);
-
-app.get('/api/shorturl', function(req, res) {
-  res.json({
-    
-  });
 });
 
 app.listen(port, function() {
